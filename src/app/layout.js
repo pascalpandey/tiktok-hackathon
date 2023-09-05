cbada13fb4ed60cef600aed3e755d084fa31e48e
jensonjenkins <dave.martheen@gmail.com>
2023-09-05 21:35:27 +0800

    change font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,10 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Noto_Sans } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const noto_sans = Noto_Sans({ 
+  weight:['100', '200', '300', '400', '500', '600', '700', '800'],
+  style:['normal', 'italic'],
+  subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={noto_sans.className}>{children}</body>
     </html>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -4,20 +4,11 @@ import UploadImage from "../components/uploadImage";
 import Image from "next/image";
 
 export default async function Home() {
-  const prisma = new PrismaClient();
-  const images = await prisma.image.findMany({});
+  
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <UploadImage />
-      {images &&
-        images.reverse().map((image) => {
-          return (
-            <>
-              <Image src={image.url} width="300" height="300" alt="deleted"/>
-            </>
-          );
-        })}
+    <main className="">
+     
     </main>
   );
 }
