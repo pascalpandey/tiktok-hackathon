864af40fcbfe2e1bbf0c39ea05211aad18f50db9
BebekJK <kevinjk2802@gmail.com>
2023-09-06 03:46:46 +0800

    restore deleted

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,11 @@
+import { Toaster } from 'react-hot-toast'
 import './globals.css'
 import { Noto_Sans } from 'next/font/google'
 import Navbar from '../components/NavBar'
 import SearchBar from '../components/SearchBar'
 import { Inter } from 'next/font/google'
+import TanstackQueryProvider from "../components/tanstackQueryProvider"
+
 const noto_sans = Noto_Sans({ 
   weight:['100', '200', '300', '400', '500', '600', '700', '800'],
   subsets: ['latin'] 
@@ -25,8 +28,11 @@ export default function RootLayout({ children }) {
         */}
         <SearchBar/>
         <div className='flex flex-row'>
+          <Toaster/>
+          <TanstackQueryProvider>
           <Navbar/>
           {children}
+          </TanstackQueryProvider>
         </div>
       </body>
     </html>
