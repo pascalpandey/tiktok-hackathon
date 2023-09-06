031e47e44df8195f14c98a5c8de99b9b83a2410e
jensonjenkins <dave.martheen@gmail.com>
2023-09-06 22:59:43 +0800

    add page anchor

--- src/app/user/[userId]/[products]/components/ShopNavbar.jsx
+++ src/app/user/[userId]/[products]/components/ShopNavbar.jsx
@@ -4,20 +4,17 @@ import Link from "next/link";
 
 export default function ShopNavbar() {
     return (
-        <div className="w-full">
-            <div className="mx-auto w-480">
+        <div className="fixed w-full bg-white">
+            <div className="mx-auto w-fit" >
                 <div className='flex'>
-                    {/* JK blh bantu routing + file structurenya g, thanks! */}
-                    <Link href="/products" className="font-medium hover:text-gray-700 border-gray-700 px-5 py-3 text-gray-500 hover:text-gray-700">
+
+                    <Link href="#Details-tag" className="font-medium hover:text-gray-700 border-gray-700 px-5 py-3 text-gray-500 ">
                         Details
                     </Link>
-                    <Link href="/testimonies" className="font-medium hover:text-gray-700  border-gray-700 text-gray-500 px-5 py-3 hover:text-gray-700">
+                    <Link href="#Reviews-tag" className="font-medium hover:text-gray-700  border-gray-700 text-gray-500 px-5 py-3 ">
                         Reviews
                     </Link>
-                    <Link href="/testimonies" className="font-medium hover:text-gray-700  border-gray-700 text-gray-500 px-5 py-3 hover:text-gray-700">
-                        Discussion
-                    </Link>
-                    <Link href="/live" className="font-medium hover:text-gray-700  border-gray-700 text-gray-500 px-5 py-3 hover:text-gray-700">
+                    <Link href="#Recommended-tag" className="font-medium hover:text-gray-700  border-gray-700 text-gray-500 px-5 py-3 ">
                         Recommended
                     </Link>
                 </div>
