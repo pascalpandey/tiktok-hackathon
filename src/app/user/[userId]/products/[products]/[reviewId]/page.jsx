8f642ba07e6d38b2062ca4d91d93dee63f0b83c8
pascal pandey <pascalis.pandey@gmail.com>
2023-09-08 21:54:33 +0800

    new file:   src/app/api/item/reviews/route.js 	new file:   src/app/user/[userId]/products/[products]/[reviewId]/page.jsx 	new file:   src/app/user/[userId]/products/[products]/components/infiniteScrollingReviews.js 	new file:   src/app/user/[userId]/reviews/[reviewId]/page.jsx

---
+++ src/app/user/[userId]/products/[products]/[reviewId]/page.jsx
@@ -0,0 +1,85 @@
+"use client"
+
+import React from "react";
+import Review from '../../../../components/Review'
+import { useInfiniteQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useRef, useEffect } from "react"
+import { useIntersection } from "@mantine/hooks";
+import { Skeleton } from "@mui/material";
+import { usePathname } from "next/navigation";
+
+export default function ReviewPage() {
+  const path = usePathname().split('/');
+  const { data, fetchNextPage, isLoading } = useInfiniteQuery(
+    [`getItemReviews${path[5]}`],
+    async ({ pageParam = 1 }) => {
+      const res = await axios.get(`http://localhost:3000/api/item/reviews?reviewId=${path[5]}&take=${1}&skip=${(pageParam - 1)}`)
+      return res
+    }, {
+    getNextPageParam: (_, pages) => {
+      return pages.length + 1
+    },
+  })
+
+  const lastReviewRef = useRef(null)
+
+  const { ref, entry } = useIntersection({
+    root: lastReviewRef.current,
+    threshold: 1
+  })
+
+  useEffect(() => {
+    if (entry?.isIntersecting) fetchNextPage()
+  }, [entry])
+
+  const reviews = data?.pages.flatMap((page) => page.data)
+  const skeletonArray = Array.from({ length: 1 });
+
+  return (
+    <main className="mx-auto pr-[170px] gap-4">
+      {isLoading ?
+        skeletonArray.map((_, i) => (
+          <div className="mt-4 pr-[65px]">
+            <Skeleton variant="rounded" animation="wave" width={350} height={600} />
+          </div>
+        ))
+        : reviews?.map((review, i) => {
+          if (i === reviews.length - 1) return (
+            <div ref={ref}>
+              <Review
+                username={review.user.username}
+                shop={review?.user?.name ?? ""}
+                desc={review.description}
+                itemName={review.item.name}
+                rating={review.rating}
+                reviewId={review.reviewId}
+                videoUrl={review.videoUrl}
+                userImgUrl={review.user?.imgUrl ?? ""}
+                itemId={review.item.itemId}
+                shared={100}
+                comments={123}
+                like={5232}
+                bm={13}
+              />
+            </div>
+          )
+          return <Review
+            username={review.user.username}
+            shop={review?.user?.name ?? ""}
+            desc={review.description}
+            itemName={review.item.name}
+            rating={review.rating}
+            reviewId={review.reviewId}
+            videoUrl={review.videoUrl}
+            userImgUrl={review.user?.imgUrl ?? ""}
+            itemId={review.item.itemId}
+            shared={100}
+            comments={123}
+            like={5232}
+            bm={13}
+          />
+        })}
+    </main>
+  )
+}
